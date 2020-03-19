50cf43b2c5684d16124923840945db588d124875
Johnny <johnny.gor10@gmail.com>
2020-03-19 17:47:55 +0800

    rabbitmq&redis

--- src/main/java/com/johnny/hm/HmApplication.java
+++ src/main/java/com/johnny/hm/HmApplication.java
@@ -1,10 +1,13 @@
 package com.johnny.hm;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-
+@EnableRabbit
+@EnableCaching
 @EnableWebSocketMessageBroker
 @SpringBootApplication
 @MapperScan(basePackages = "com.johnny.hm.mapper")
